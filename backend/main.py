abe16f49f0376de9fa6300970975c943e3cefd88
mirzafar <m_mirzafar@mail.ru>
2025-09-09 18:03:42 +0500

    port

--- backend/main.py
+++ backend/main.py
@@ -2664,4 +2664,4 @@ async def get_calendar_dispensing_day(
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8078)
+    uvicorn.run(app, host="127.0.0.1", port=8887)
